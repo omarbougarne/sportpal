8ef6144d4fe41d1571ae9ee2922c08f718042432
omarbougarne <omarbougarne98@gmail.com>
2025-01-30 14:01:37 +0100

    Creating tests

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -1,18 +1,44 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersService } from './users.service';
+import { User } from './schema/users.schema';
+import { getModelToken } from '@nestjs/mongoose';
+import { Module } from '@nestjs/common';
 
 describe('UsersService', () => {
   let service: UsersService;
+  let userModel: User;
 
   beforeEach(async () => {
+    const mockUserModel = {
+      create: jest.fn(),
+      findUser: jest.fn()
+    };
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
+      providers: [
+        UsersService,
+        {
+        provide: getModelToken(User.name),
+        useValue: mockUserModel,
+        },
+      ],
     }).compile();
 
     service = module.get<UsersService>(UsersService);
+    userModel = module.get(getModelToken(User.name))
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  describe('create', () =>{
+    it('Should create a unique user with hashed password', async () => {
+      const createUserDto = {
+        name: 'omar',
+        email: 'omar@email.com',
+        password: 'supersecure'
+      };
+
+    })
+  })
 });
