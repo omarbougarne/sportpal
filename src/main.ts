b96a1d52900da3e5d069f897ac2b71656bfb01f8
omarbougarne <omarbougarne98@gmail.com>
2025-01-30 12:35:57 +0100

    Fix Port error made a loop to loop through ports if one is busy

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
+  const logger = new Logger('Bootstrap')
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const defaultPort = await parseInt(process.env.PORT, 10) || 5000;
+  const maxRetries = 6;
+  let port = defaultPort;
+
+  for(let i = 0; i < maxRetries; i++){
+      try{
+      await app.listen(port);
+      logger.log(`Application connected on port ${port}`);
+      break;
+    }catch(error){
+      logger.log(`Trying port ${port + 1}`)
+      port += 1
+    }
+  }
 }
 bootstrap();
