20562170934d0fb9180813fe9bf4579f92dc01db
omarbougarne <omarbougarne98@gmail.com>
2025-01-30 12:48:18 +0100

    Feat added Exception handling in user service Description added try catch blocks to catch possible errors

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { User, UserDocument } from './schema/users.schema';
@@ -10,6 +10,7 @@ export class UsersService {
     constructor(@InjectModel(User.name) private userModel: Model<UserDocument>){}
     
     async create(createUserDto: CreateUSerDto): Promise<User>{
+    try{
         const salt = await bcrypt.genSalt()
         const hashedPassword = await bcrypt.hash(createUserDto.password, salt)
         const createUser = new this.userModel({
@@ -17,9 +18,20 @@ export class UsersService {
             password: hashedPassword
         })
         return createUser.save()
+    }catch(error){
+        throw new HttpException('Error Creating user', HttpStatus.INTERNAL_SERVER_ERROR)
+    }
     }
 
     async findUser(email: string): Promise<User | undefined>{
-        return this.userModel.findOne({email}).exec()
+        try{
+        const user = await this.userModel.findOne({email}).exec()
+        if(!user){
+            throw new HttpException('User Not Found!', HttpStatus.NOT_FOUND)
+        }
+        return user
+    }catch(error){
+        throw new HttpException('Error Finding User', HttpStatus.INTERNAL_SERVER_ERROR)
+    }
     }
 }   
