14420493e196bc722a50061c960485480929107b
omarbougarne <omarbougarne98@gmail.com>
2025-01-30 05:13:09 +0100

    Feat user schema created

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
@@ -11,6 +12,7 @@ import { ConfigModule } from '@nestjs/config';
       envFilePath: '.env', 
       isGlobal: true
     }),
+    MongooseModule.forRoot(process.env.MONGODB_URI),
       UsersModule,
       AuthModule],
   controllers: [AppController],
---
+++ src/users/schema/users.schema.ts
@@ -0,0 +1,29 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose'
+import { Role } from '../enums/role.enum'
+
+
+export type UserDocument = User & Document;
+
+@Schema({
+  timestamps: true,
+})
+
+export class User{
+    @Prop({ required: true})
+    name: string;
+
+    @Prop({ required: false })
+    displayName: string;
+
+    @Prop({ required: true})
+    email: string;
+
+    @Prop({ required: true})
+    password: string;
+
+    @Prop({ type: String, enum: Role, default: Role.User })
+    role: Role
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
