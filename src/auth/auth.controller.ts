6920f66591018920c0203736f1e395cab814828c
omarbougarne <omarbougarne98@gmail.com>
2025-02-01 07:29:45 +0100

    small-fix

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Body, Request } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateUserDto } from 'src/users/dto/create.user.dto';
 
