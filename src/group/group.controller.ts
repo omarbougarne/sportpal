25a28fe75178d7959eb0ce934f676bc5dea80c87
omarbougarne <omarbougarne98@gmail.com>
2025-02-02 00:01:59 +0100

    Group controller methods for join andcreation

--- src/group/group.controller.ts
+++ src/group/group.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { GroupService } from './group.service';
+import { CreateGroupDto } from './dto/create-group.dto';
+import { JoinGroupDto } from './dto/join-group.dto';
 
 @Controller('group')
-export class GroupController {}
+export class GroupController {
+    constructor(private groupService: GroupService) { }
+
+    @Post('create')
+    async createGroup(@Body() createGroupDto: CreateGroupDto) {
+        return this.groupService.createGroup(createGroupDto);
+    }
+
+    @Post('join')
+    async joinGroup(@Body() joinGroupDto: JoinGroupDto) {
+        return this.groupService.joinGroup(joinGroupDto);
+    }
+}
