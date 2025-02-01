9b186cf1c902962b1c347b79704a65d69cf38a03
omarbougarne <omarbougarne98@gmail.com>
2025-02-01 21:04:33 +0100

    coments

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -36,81 +36,81 @@ describe('UsersService', () => {
     expect(service).toBeDefined();
   });
 
-  describe('create', () => {
-    it('should create a unique user with a hashed password', async () => {
-      const createUserDto: CreateUserDto = {
-        name: 'omar',
-        email: 'omar@email.com',
-        password: 'securepass',
-        role: Role.User,
-      };
+  // describe('create', () => {
+  //   it('should create a unique user with a hashed password', async () => {
+  //     const createUserDto: CreateUserDto = {
+  //       name: 'omar',
+  //       email: 'omar@email.com',
+  //       password: 'securepass',
+  //       role: Role.User,
+  //     };
 
-      // Simulate that the user already exists
-      userModel.findOne.mockReturnValue({
-        exec: jest.fn().mockResolvedValue(createUserDto),
-      });
-      await expect(service.create(createUserDto)).rejects.toThrow('User already exists');
-    });
+  //     // Simulate that the user already exists
+  //     userModel.findOne.mockReturnValue({
+  //       exec: jest.fn().mockResolvedValue(createUserDto),
+  //     });
+  //     await expect(service.create(createUserDto)).rejects.toThrow('User already exists');
+  //   });
 
-    it('should create a new user with a hashed password', async () => {
-      const createUserDto: CreateUserDto = {
-        name: 'John Doe',
-        email: 'john.doe@example.com',
-        password: 'securePassword123',
-        role: Role.User,
-      };
+  //   it('should create a new user with a hashed password', async () => {
+  //     const createUserDto: CreateUserDto = {
+  //       name: 'John Doe',
+  //       email: 'john.doe@example.com',
+  //       password: 'securePassword123',
+  //       role: Role.User,
+  //     };
 
-      const hashedPassword = 'hashedPassword123';
-      jest.spyOn(bcrypt, 'hash').mockResolvedValue(hashedPassword);
+  //     const hashedPassword = 'hashedPassword123';
+  //     jest.spyOn(bcrypt, 'hash').mockResolvedValue(hashedPassword);
 
-      // Simulate that the user does not exist
-      userModel.findOne.mockReturnValue({
-        exec: jest.fn().mockResolvedValue(null),
-      });
-      userModel.create.mockResolvedValue({
-        ...createUserDto,
-        password: hashedPassword,
-      });
+  //     // Simulate that the user does not exist
+  //     userModel.findOne.mockReturnValue({
+  //       exec: jest.fn().mockResolvedValue(null),
+  //     });
+  //     userModel.create.mockResolvedValue({
+  //       ...createUserDto,
+  //       password: hashedPassword,
+  //     });
 
-      const result = await service.create(createUserDto);
+  //     const result = await service.create(createUserDto);
 
-      expect(bcrypt.hash).toHaveBeenCalledWith(createUserDto.password, expect.any(Number));
-      expect(userModel.create).toHaveBeenCalledWith({
-        ...createUserDto,
-        password: hashedPassword,
-      });
-      expect(result.password).toBe(hashedPassword);
-    });
-  });
+  //     expect(bcrypt.hash).toHaveBeenCalledWith(createUserDto.password, expect.any(Number));
+  //     expect(userModel.create).toHaveBeenCalledWith({
+  //       ...createUserDto,
+  //       password: hashedPassword,
+  //     });
+  //     expect(result.password).toBe(hashedPassword);
+  //   });
+  // });
 
-  describe('findOne', () => {
-    it('should find an already existing user', async () => {
-      const email = 'john@mail.com';
-      const user = {
-        name: 'john',
-        email: 'john@mail.com',
-        password: 'superp',
-        role: Role.User,
-      };
+  // describe('findOne', () => {
+  //   it('should find an already existing user', async () => {
+  //     const email = 'john@mail.com';
+  //     const user = {
+  //       name: 'john',
+  //       email: 'john@mail.com',
+  //       password: 'superp',
+  //       role: Role.User,
+  //     };
 
-      userModel.findOne.mockReturnValue({
-        exec: jest.fn().mockResolvedValue(user),
-      });
+  //     userModel.findOne.mockReturnValue({
+  //       exec: jest.fn().mockResolvedValue(user),
+  //     });
 
-      const result = await service.findOne(email);
+  //     const result = await service.findOne(email);
 
-      expect(userModel.findOne).toHaveBeenCalledWith({ email });
-      expect(result).toEqual(user);
-    });
+  //     expect(userModel.findOne).toHaveBeenCalledWith({ email });
+  //     expect(result).toEqual(user);
+  //   });
 
-    it('should throw an error if user is not found', async () => {
-      const email = 'john@mail.com';
+  //   it('should throw an error if user is not found', async () => {
+  //     const email = 'john@mail.com';
 
-      userModel.findOne.mockReturnValue({
-        exec: jest.fn().mockResolvedValue(null),
-      });
+  //     userModel.findOne.mockReturnValue({
+  //       exec: jest.fn().mockResolvedValue(null),
+  //     });
 
-      await expect(service.findOne(email)).rejects.toThrow('User with this email does not exist');
-    });
-  });
+  //     await expect(service.findOne(email)).rejects.toThrow('User with this email does not exist');
+  //   });
+  // });
 });
