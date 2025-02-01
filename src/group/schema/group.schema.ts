3bd1724c1f609df04410388c0853ce36ab48b32b
omarbougarne <omarbougarne98@gmail.com>
2025-02-01 22:43:23 +0100

    Feat creation of group schema + fix in user schema

---
+++ src/group/schema/group.schema.ts
@@ -0,0 +1,26 @@
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import { Types, Document } from "mongoose";
+
+export type GroupDocument = Group & Document;
+
+@Schema({
+    timestamps: true,
+})
+
+export class Group {
+
+    @Prop({ required: true })
+    name: string
+
+    @Prop({ type: [{ type: Types.ObjectId, ref: 'User' }], default: [] })
+    @Prop({ required: false })
+    sport?: string
+
+    @Prop({ required: false })
+    activity?: string
+
+    @Prop({ required: true })
+    location: string
+}
+
+export const GroupSchema = SchemaFactory.createForClass(Group)
--- src/users/schema/users.schema.ts
+++ src/users/schema/users.schema.ts
@@ -9,36 +9,36 @@ export type UserDocument = User & Document;
   timestamps: true,
 })
 
-export class User{
-    @Prop({ required: true})
-    name: string;
+export class User {
+  @Prop({ required: true })
+  name: string;
 
-    @Prop({ required: false })
-    displayName: string;
+  @Prop({ required: false })
+  displayName?: string;
 
-    @Prop({ required: true})
-    email: string;
+  @Prop({ required: true })
+  email: string;
 
-    @Prop({ required: true})
-    password: string;
+  @Prop({ required: true })
+  password: string;
 
-    @Prop({type: [String], required: false})
-    favoriteSports?: string
-    
-    @Prop({type: String, required: false})
-    athleticLevel?: string
-    
-    @Prop({type: [String], required: false})
-    disponsibility?: string
-    
-    @Prop({type: String, required: false})
-    location?: string
-    
-    @Prop({ type: String, enum: Role, default: Role.User })
-    role: Role
+  @Prop({ type: [String], required: false })
+  favoriteSports?: string
 
-    @Prop({type: Date, default: null})
-    deletedAt: Date | null
+  @Prop({ type: String, required: false })
+  athleticLevel?: string
+
+  @Prop({ type: [String], required: false })
+  disponsibility?: string
+
+  @Prop({ type: String, required: false })
+  location?: string
+
+  @Prop({ type: String, enum: Role, default: Role.User })
+  role: Role
+
+  @Prop({ type: Date, default: null })
+  deletedAt: Date | null
 }
 
 export const UserSchema = SchemaFactory.createForClass(User);
