ec298b1b6ebce3d6c9b98077ad48d0edabce54c4
omarbougarne <omarbougarne98@gmail.com>
2025-02-02 23:39:39 +0100

    Fixing and returning to using id in param instead of request

--- src/group/group.controller.ts
+++ src/group/group.controller.ts
@@ -1,14 +1,15 @@
-import { Body, Controller, Param, Post } from '@nestjs/common';
+import { Controller, Post, Body, UseGuards, Req, Param } from '@nestjs/common';
 import { GroupService } from './group.service';
 import { CreateGroupDto } from './dto/create-group.dto';
 import { JoinGroupDto } from './dto/join-group.dto';
+import { Request } from 'express';
 
-@Controller('group')
+@Controller('groups')
 export class GroupController {
-    constructor(private groupService: GroupService) { }
+    constructor(private readonly groupService: GroupService) { }
 
-    @Post('create')
-    async createGroup(@Param() userId: string, @Body() createGroupDto: CreateGroupDto) {
+    @Post('create/:userId')
+    async createGroup(@Param('userId') userId: string, @Body() createGroupDto: CreateGroupDto) {
         return this.groupService.createGroup(createGroupDto, userId);
     }
 
--- src/group/group.module.ts
+++ src/group/group.module.ts
@@ -3,12 +3,14 @@ import { GroupService } from './group.service';
 import { GroupController } from './group.controller';
 import { Group, GroupSchema } from './schema/group.schema';
 import { MongooseModule } from '@nestjs/mongoose';
+import { UsersService } from 'src/users/users.service';
+import { User, UserSchema } from 'src/users/schema/users.schema';
 
 @Module({
   imports: [
-    MongooseModule.forFeature([{ name: Group.name, schema: GroupSchema }])
+    MongooseModule.forFeature([{ name: Group.name, schema: GroupSchema }, { name: User.name, schema: UserSchema }])
   ],
-  providers: [GroupService],
+  providers: [GroupService, UsersService],
   controllers: [GroupController]
 })
 export class GroupModule { }
