8ec5ce10d07fb9fb51357e1226b9888bb9733e4d
omarbougarne <omarbougarne98@gmail.com>
2025-02-03 02:31:20 +0100

    Feat adding update and delete methods and list groups

--- src/group/group.service.ts
+++ src/group/group.service.ts
@@ -48,4 +48,53 @@ export class GroupService {
             throw new HttpException('Error creating group', HttpStatus.INTERNAL_SERVER_ERROR)
         }
     }
+
+    async getGroupById(groupId: string): Promise<{ data: Group }> {
+        try {
+            const group = await this.groupModel.findById(groupId).exec();
+            if (!group) {
+                throw new HttpException('Group not found', HttpStatus.NOT_FOUND);
+            }
+            return { data: group };
+        } catch (error) {
+            this.logger.error('Error fetching group by ID', error.stack)
+            throw new HttpException('Error fetching group by Id', HttpStatus.INTERNAL_SERVER_ERROR)
+        }
+    }
+
+    async updateGroup(groupId: string, updateGroupDto: CreateGroupDto): Promise<{ data: Group }> {
+        try {
+            const updateGroup = await this.groupModel.findByIdAndUpdate(groupId, updateGroupDto, { new: true })
+            if (!updateGroup) {
+                throw new HttpException('Group not found', HttpStatus.NOT_FOUND)
+            }
+            return { data: updateGroup }
+        } catch (error) {
+            this.logger.error('Error updating group', error.stack)
+            throw new HttpException('Error updating group', HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    async deleteGroup(groupId: string, updateGroup: CreateGroupDto): Promise<{ data: Group }> {
+        try {
+            const deleteGroup = await this.groupModel.findByIdAndDelete(groupId, updateGroup)
+            if (!deleteGroup) {
+                throw new HttpException('Group not found', HttpStatus.NOT_FOUND)
+            }
+            return { data: deleteGroup }
+        } catch (error) {
+            this.logger.error('Error updating group', error.stack)
+            throw new HttpException('Error updating group', HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    async getAllGroups(): Promise<{ data: Group[] }> {
+        try {
+            const groups = await this.groupModel.find().exec();
+            return { data: groups };
+        } catch (error) {
+            this.logger.error('Error fetching all groups', error.stack);
+            throw new HttpException('Error fetching all groups', HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 }
