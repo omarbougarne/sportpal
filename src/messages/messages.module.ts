ab67e7cb14cfdd215413db7b41020c34b509ef6d
omarbougarne <omarbougarne98@gmail.com>
2025-02-26 10:10:51 +0000

    Feat implementation in appModule of messages module and configuration of message module

--- src/messages/messages.module.ts
+++ src/messages/messages.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { MessageGateway } from './messages.gateway';
 import { MessagesService } from './messages.service';
+import { Messages, MessagesSchema } from './schema/messages.schema';
+import { GroupModule } from '../group/group.module';
 import { MessagesController } from './messages.controller';
 
 @Module({
-  providers: [MessagesService],
-  controllers: [MessagesController]
+  imports: [
+    MongooseModule.forFeature([{ name: Messages.name, schema: MessagesSchema }]),
+    GroupModule,
+  ],
+  providers: [MessageGateway, MessagesService],
+  controllers: [MessagesController],
+  exports: [MessagesService],
 })
-export class MessagesModule {}
+export class MessagesModule { }
