21750f6be5cffa36d58a87ef9460963abe381faa
omarbougarne <omarbougarne98@gmail.com>
2025-03-07 22:35:34 +0000

    Feat and fix dpendencies in trainer module

--- src/trainer/trainer.module.ts
+++ src/trainer/trainer.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { TrainerService } from './trainer.service';
 import { TrainerController } from './trainer.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Trainer, TrainerSchema } from './schema/tariner.schema';
+import { UsersService } from 'src/users/users.service';
+import { User, UserSchema } from 'src/users/schema/users.schema';
 
 @Module({
-  providers: [TrainerService],
+  imports: [
+    MongooseModule.forFeature([{ name: Trainer.name, schema: TrainerSchema }, { name: User.name, schema: UserSchema }]),
+  ],
+  providers: [TrainerService, UsersService],
   controllers: [TrainerController]
 })
-export class TrainerModule {}
+export class TrainerModule { }
