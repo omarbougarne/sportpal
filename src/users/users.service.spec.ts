6edbeff662f0ab193828f926f69bf15f4659e53d
omarbougarne <omarbougarne98@gmail.com>
2025-03-19 02:25:02 +0000

    Feat fixes for ci pipleine

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -2,9 +2,7 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { getModelToken } from '@nestjs/mongoose';
 import { UsersService } from './users.service';
 import { User } from './schema/users.schema';
-import { CreateUserDto } from './dto/create-user.dto';
-import { Role } from './enums/role.enum';
-import * as bcrypt from 'bcrypt';
+
 
 describe('UsersService', () => {
   let service: UsersService;
